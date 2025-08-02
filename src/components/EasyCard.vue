47fd50ac8d9cfa93bae24ae15f964f22a549b63d
TWTPEN1608033S2\USER <s881672000@gmail.com>
2025-08-02 18:46:52 +0800

    props emit 練習

--- src/components/EasyCard.vue
+++ src/components/EasyCard.vue
@@ -1,10 +1,19 @@
 <script setup>
 import { ref } from 'vue';
+import { defineProps } from 'vue';
 
 const imgUrl = ref('https://thumb.photo-ac.com/e7/e71cbebbdd8c2130a037b78c2d3f99d4_t.jpeg')
-const num = ref(1);
-const add = ()=>{
-    num.value++ //卡片的數字狀態各自獨立
+// const num = ref(1);
+// const add = ()=>{
+//     num.value++ //卡片的數字狀態各自獨立
+// }
+
+defineProps(['content'])
+
+const emit = defineEmits(['emitTitle'])
+const editOuterTitle= ()=>{
+    console.log("內層觸發emit");
+    emit('emitTitle','內層要修改外層的值')
 }
 </script>
 
@@ -12,9 +21,11 @@ const add = ()=>{
     <div class="card">
         <img :src="imgUrl" alt="..."/>
         <div class="card-body">
-            <h5 class="card-title">card title {{ num }}</h5>
-            <p class="card-text">AGHHDJWDBWJDKQWJEQNDQJNQKJDNJWNJDNWJNDJQNBDBHghfdjwq12</p>
-            <a href="#" class="btn btn-primary" @click.prevent="add">Go somewhere</a>
+            <!-- <h5 class="card-title">card title {{ num }}</h5> -->
+            <h5 class="card-title">{{ content.title }}</h5>
+            <p class="card-text">{{ content.content }}</p>
+            <!-- <a href="#" class="btn btn-primary" @click.prevent="add">Go somewhere</a> -->
+            <a href="#" class="btn btn-primary" @click.prevent="editOuterTitle">Go somewhere</a>
         </div>
     </div>
 </template>
--- src/views/EasySample.vue
+++ src/views/EasySample.vue
@@ -3,15 +3,30 @@
 import EasyHeader from '@/components/EasyHeader.vue';
 import EasyCard from '@/components/EasyCard.vue';
 import EasyFooter from '@/components/EasyFooter.vue';
+import { ref } from 'vue';
+
+const data = ref({
+    title: '這是一張卡片',
+    content:"ejfehfjknsdjfknwirewfnwlfklsd",
+    imgUrl:'https://thumb.photo-ac.com/e7/e71cbebbdd8c2130a037b78c2d3f99d4_t.jpeg'
+})
+
+const editTitle = (newTitle) =>{
+    console.log('editTitle:', newTitle);
+    data.value.title = newTitle;
+}
+
+//props: 前內後外 通常會使用同名。單向數據流。內層資料不能改外層。
 </script>
 
 <template>
     <div class="container">
-        <EasyHeader/>
-        <div class="row">
-            <div class="col-4" v-for="i in 10" :key="i">
-             <EasyCard/>  
-            </div>
+        <EasyHeader/> 
+        <div class="col-4">
+        <!-- <div class="row"> -->
+            <!-- <div class="col-4" v-for="i in 10" :key="i"> -->
+             <EasyCard v-bind:content="data" @emit-title="editTitle"/>  
+            <!-- </div> -->
         </div>
         <EasyFooter/>
     </div>
